c7e72af073864ebf7d89ef5925c738c14c4dd139
sandrardias <89422150+sandrardias@users.noreply.github.com>
2021-10-31 13:25:38 -0700

    Add files via upload

---
+++ chapter04/magicians.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+	message = f"{magician.title()}, that was a great trick!"
+	print(message)
+	message = f"I cant't wait to see your next trick, {magician.title()}.\n"
+	print(message)
+print("Thank you, everyone. That's was a great magic show!")
